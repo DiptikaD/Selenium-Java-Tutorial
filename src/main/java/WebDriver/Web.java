bb94453412dbb2068167ffb0a79b026eefc76822
DiptikaD <diptikadevi@gmail.com>
2025-03-03 14:38:27 -0500

    psvm and driver located

--- src/main/java/WebDriver/Web.java
+++ src/main/java/WebDriver/Web.java
@@ -4,4 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 public class Web {
+    public static void main(String[] args) {
+        // System property for EdgeDriver, can use any other browser driver here
+        System.setProperty("webdriver.edge.driver", "C:/Users/DiptikaDevi/Downloads/edgedriver_win64/msedgedriver.exe");
+
+        WebDriver driver = new EdgeDriver();
+    }
 }
